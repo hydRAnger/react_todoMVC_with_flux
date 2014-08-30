0738443d8d2783222cc9f1f6f848c4b14dd3593d
hydra <hydra@icemaple.com>
2014-08-30 22:37:07 +0800

    fix TodoStore.js bug

--- js/stores/TodoStore.js
+++ js/stores/TodoStore.js
@@ -6,10 +6,7 @@ var merge = require('react/lib/merge');
 var _todo_list = {}; // collection of todo items
 var EVT_CHANGE = 'change';
 
-/**
- * Create a TODO item.
- * @param {string} text The content of the TODO
- */
+// Create a TODO item.
 function create(content) {
   var timestamp_as_id = Date.now();
   _todo_list[timestamp_as_id] = {
@@ -19,10 +16,16 @@ function create(content) {
   };
 }
 
-/**
- * Delete a TODO item.
- * @param {string} id
- */
+function update(id, updates) {
+    _todos[id] = merge(_todos[id], updates);
+}
+
+function updateAll(updates) {
+  for (var id in _todos) {
+    update(id, updates);
+  }
+}
+
 function destroy(id) {
   if (_todo_list[id]) {
     delete _todo_list[id];
@@ -58,27 +61,83 @@ var TodoStore = merge(EventEmitter.prototype, {
     this.removeListener(EVT_CHANGE, callback);
   }
 
-  dispatcherIndex : AppDispatcher.register(function(payload) {
-    var action = payload.action;
+  //dispatcherIndex : AppDispatcher.register(function(payload) {
+  //  var action = payload.action;
 
-    switch(action.action_type) {
-      case TodoConstants.TODO_CREATE:
-        var content = action.text.trim() 
-        if (content !== '') {
-          create(content);
-          TodoStore.emit_change();
-        }
-        break;
+  //  switch(action.action_type) {
+  //    case TodoConstants.TODO_CREATE:
+  //      var content = action.text.trim() 
+  //      if (content !== '') {
+  //        create(content);
+  //        TodoStore.emit_change();
+  //      }
+  //      break;
 
-      case TodoConstants.TODO_DESTROY:
-        destroy(action.id);
-        TodoStore.emit_change();
-        break;
-    }
+  //    case TodoConstants.TODO_DESTROY:
+  //      destroy(action.id);
+  //      TodoStore.emit_change();
+  //      break;
+  //  }
 
-    return true; 
-  })
+  //  return true; 
+  //})
 
+});
+
+AppDispatcher.register(function(payload) {
+  var action = payload.action;
+  var text;
+
+  switch(action.actionType) {
+    case TodoConstants.TODO_CREATE:
+      text = action.text.trim();
+      if (text !== '') {
+        create(text);
+      }
+      break;
+
+    case TodoConstants.TODO_TOGGLE_COMPLETE_ALL:
+      if (TodoStore.areAllComplete()) {
+        updateAll({complete: false});
+      } else {
+        updateAll({complete: true});
+      }
+      break;
+
+    case TodoConstants.TODO_UNDO_COMPLETE:
+      update(action.id, {complete: false});
+      break;
+
+    case TodoConstants.TODO_COMPLETE:
+      update(action.id, {complete: true});
+      break;
+
+    case TodoConstants.TODO_UPDATE_TEXT:
+      text = action.text.trim();
+      if (text !== '') {
+        update(action.id, {text: text});
+      }
+      break;
+
+    case TodoConstants.TODO_DESTROY:
+      destroy(action.id);
+      break;
+
+    case TodoConstants.TODO_DESTROY_COMPLETED:
+      destroyCompleted();
+      break;
+
+    default:
+      return true;
+  }
+
+  // This often goes in each case that should trigger a UI change. This store
+  //   // needs to trigger a UI change after every view action, so we can make the
+  //     // code less repetitive by putting it here.  We need the default case,
+  //       // however, to make sure this only gets called after one of the cases above.
+  TodoStore.emitChange();
+
+  return true; // No errors.  Needed by promise in Dispatcher.
 });
 
 module.exports = TodoStore;
